f2f06fc282dc1e7ffb6782fe77f760c1e4d99f4f
PeterSamehShafik <petersameh953@gmail.com>
2026-07-07 22:49:43 +0300

    prod:prepare for production

--- bundle-builder/src/api/axios.ts
+++ bundle-builder/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://smart-home-builder-be.vercel.app/',
   headers: {
     'Content-Type': 'application/json',
   },
--- bundle-builder/src/main.tsx
+++ bundle-builder/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import "./app.css";
+import "./App.css";
 import App from "./App.tsx";
 import { AppProvider } from "./providers/AppProvider.tsx";
 import { Navbar } from "./components/shared/Navbar.tsx";
